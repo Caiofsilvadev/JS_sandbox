efae8e80186931cff872296e5c7a948b2dc59999
Caio Fernando da Silva <caiofsilvadev@gmail.com>
2022-12-17 12:49:57 -0300

    Criação de repositorio para estudo de javascript

---
+++ app.js
@@ -0,0 +1,57 @@
+/*
+console.log('Hello');
+console.log(123);
+console.log(true);
+var greeting = 'Hello';
+console.log(greeting);
+console.log([1, 2, 3, 4]);
+console.log({ a: 1, b: 2 });
+console.table({ a: 1, b: 2 });
+console.error('error');
+console.clear();
+console.warn('error');
+*/
+/*
+var name = 'John Doe';
+console.log(name);
+name = 'steve smith';
+console.log(name);
+
+//init var
+var greeting;
+console.log(greeting);
+greeting = 'Hello';
+console.log(greeting);
+
+//letters,  numbers, _, $
+//Can not start with number
+
+//Multi word Vars
+var firstName = 'john'; //Camel Case
+var FirstName = 'Sara'; //pascal Case
+var first_name = 'Tom'; //Underscore
+
+*/
+/*
+//Let
+let name = 'John Doe';
+console.log(name);
+name = 'steve smith';
+console.log(name);
+*/
+
+//Const
+//const name = 'john';
+//console.log(name);
+//Can not reassign
+//Have tp assign a value;
+
+const person = { name: 'john', age: 30 }
+
+person.name = 'Sara';
+person.age = 32;
+
+const numbers = [1, 2, 3, 4, 5];
+numbers.push(6);
+
+console.log(numbers);
